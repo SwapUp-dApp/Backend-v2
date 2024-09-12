c94c6f1b33177aee41969f1f745b100deabe7cfb
KhalilAlliedC <khalil.ahmad@alliedc.com>
2024-09-12 21:44:14 +0500

    loggedClient used for uploading image as well

--- src/api/controllers/twitterController.js
+++ src/api/controllers/twitterController.js
@@ -222,7 +222,7 @@ async function upload_image_to_twitter(req, res) {
      `.trim();
 
       // Upload the image to Twitter using SwapUp twitter client
-      const mediaId = await swapUpTwitterClient.v1.uploadMedia(buffer, { mimeType: 'image/jpeg', chunked: true });
+      const mediaId = await loggedClient.v1.uploadMedia(buffer, { mimeType: 'image/jpeg', chunked: true });
 
       console.log("Media Id: ", mediaId);
 
