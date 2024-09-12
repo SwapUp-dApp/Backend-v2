8faa31ee441406a22a04535b299ea4c22605f594
KhalilAlliedC <khalil.ahmad@alliedc.com>
2024-09-12 17:26:09 +0500

    disabled puppeteer sandbox on browser launch

--- src/api/controllers/twitterController.js
+++ src/api/controllers/twitterController.js
@@ -427,7 +427,11 @@ const getSwapHTMLContentBySwapTokens = (swapTokens, maxSwapTokenToShow) => {
 const getBufferFromHTMLString = async (htmlString) => {
    try {
 
-      const browser = await puppeteer.launch();
+      const browser = await puppeteer.launch({
+         args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      });
+
+
       const page = await browser.newPage();
 
       await page.setContent(htmlString, {
