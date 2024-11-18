38482609fc185307e356a55919cbdf20041b3ad0
Ammar Waheed <33984786+BlazingC0der@users.noreply.github.com>
2024-11-18 19:27:55 +0500

    handling proposal id in notifications table

--- src/api/utils/helpers.js
+++ src/api/utils/helpers.js
@@ -1,5 +1,5 @@
-import logger from '../../logger';
-const crypto = require('crypto');
+import logger from "../../logger"
+const crypto = require("crypto")
 import db from "../../database/models"
 
 // const fs = require('fs');
@@ -20,47 +20,54 @@ import db from "../../database/models"
 // };
 
 export function tryParseJSON(jsonString) {
-  try {
-    const parsed = JSON.parse(jsonString);
-    return parsed;
-  } catch (err) {
-    return jsonString; // Return original string if parsing fails
-  }
+    try {
+        const parsed = JSON.parse(jsonString)
+        return parsed
+    } catch (err) {
+        return jsonString // Return original string if parsing fails
+    }
 }
 
 // Webhook helper functions starts here
 
-export const isExpiredWebhook = (timestamp, expirationInSeconds,) => {
-  const currentTime = Math.floor(Date.now() / 1000);
-  return currentTime - parseInt(timestamp) > expirationInSeconds;
-};
+export const isExpiredWebhook = (timestamp, expirationInSeconds) => {
+    const currentTime = Math.floor(Date.now() / 1000)
+    return currentTime - parseInt(timestamp) > expirationInSeconds
+}
 
 export function isValidWebhookSignature(body, timestamp, signature, secret) {
-  const dataToSign = `${timestamp}.${JSON.stringify(body)}`;
-  logger.info("dataToSign: ", dataToSign);
+    const dataToSign = `${timestamp}.${JSON.stringify(body)}`
+    logger.info("dataToSign: ", dataToSign)
 
-  // Generate the HMAC SHA-256 signature
-  const expectedSignature = crypto
-    .createHmac('sha256', secret)
-    .update(dataToSign)
-    .digest('hex');
+    // Generate the HMAC SHA-256 signature
+    const expectedSignature = crypto
+        .createHmac("sha256", secret)
+        .update(dataToSign)
+        .digest("hex")
 
-  logger.info('expectedSignature: ', expectedSignature);
+    logger.info("expectedSignature: ", expectedSignature)
 
-  return crypto.timingSafeEqual(
-    Buffer.from(expectedSignature),
-    Buffer.from(signature),
-  );
+    return crypto.timingSafeEqual(
+        Buffer.from(expectedSignature),
+        Buffer.from(signature)
+    )
 }
 // Webhook helper functions ends here
 
 // helper for entering a new record in notififications table
-export const createNotification = async (receiver_address,originator_address,status,trade_id) => {
-  await db.notifications.create({
-      receiver_address,
-      originator_address,
-      status,
-      trade_id,
-      read: false
-  })
+export const createNotification = async (
+    receiver_address,
+    originator_address,
+    status,
+    trade_id,
+    proposal_id = null
+) => {
+    await db.notifications.create({
+        receiver_address,
+        originator_address,
+        status,
+        trade_id,
+        read: false,
+        proposal_id
+    })
 }
